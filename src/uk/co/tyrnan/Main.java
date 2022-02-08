9aa15202d6a8680e32718761ac8ca0d3a85d726c
Tyrnan Batch <51964350+TyrnanBatch@users.noreply.github.com>
2022-02-08 21:33:21 +0000

    Update Main.java

--- src/uk/co/tyrnan/Main.java
+++ src/uk/co/tyrnan/Main.java
@@ -1,13 +1,41 @@
 package uk.co.tyrnan;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void filewriter(String input, String location) throws IOException {
+        FileWriter fileWriter = new FileWriter(location);
+        fileWriter.write(input);
+        fileWriter.close();
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+
+        Scanner sc = new Scanner(System.in);
+
         if (!masterPassword.passwordChecker()) {
             System.out.println("Password check failed");
             System.exit(0);
         }
 
-        System.out.println("YOUR IN");
+        boolean running = true;
+
+        while (running) {
+
+            System.out.print("Command: ");
+
+            String command = sc.nextLine();
+            String[] sec = command.split(" ");
+
+            if (Objects.equals(sec[0], "add")) {
+                createPassword cp = new createPassword(sec);
+                cp.createpassword();
+            }
+        }
     }
 }
